badcc2444bb28dc3ac0c00473b714cbe3f81128e
Flavien Chenu <flavienchenu@hotmail.com>
2024-11-26 13:59:00 +0100

    feat(backend): add daemon program

---
+++ backend/Zeus.Daemon.Runner/Builder/DaemonRunnerBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Zeus.Daemon.Runner.Runner;
+
+namespace Zeus.Daemon.Runner.Builder;
+
+public class DaemonRunnerBuilder
+{
+    private string[] Args { get; set; } = [];
+    
+    public IServiceCollection Services { get; } = new ServiceCollection();
+
+    private DaemonRunnerBuilder() {}
+
+    public static DaemonRunnerBuilder CreateBuilder(string[] args)
+    {
+        return new DaemonRunnerBuilder { Args = args };
+    }
+
+    public DaemonRunner Build()
+    {
+        return new DaemonRunner(Services.BuildServiceProvider());
+    }
+}
--- backend/Zeus.Daemon.Runner/Program.cs
+++ backend/Zeus.Daemon.Runner/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Zeus.Daemon.Runner.Builder;
+
+var builder = DaemonRunnerBuilder.CreateBuilder(args);
+
+var runner = builder.Build();
+
+runner.Run();
