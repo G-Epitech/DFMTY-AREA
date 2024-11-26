01c095eec338412c2b5c01fcafbc148890d63360
Yann <yannmasson17@gmail.com>
2024-11-26 18:06:54 +0100

    refactor: move app class from main.app to app/app.dart file

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:triggo/main.dart';
+import 'package:triggo/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
