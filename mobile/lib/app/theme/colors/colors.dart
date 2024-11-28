d365ff68b8524807f28b70589dc7eba4eac57727
thomaltarix <thomas.boue@epitech.eu>
2024-11-28 14:13:03 +0100

    feat(mobile): define theme of the app
    Defining the theme consists of defining the colors and the fonts. It also consists of adding the fonts in the pubspec
    
    Signed-off-by: thomaltarix <thomas.boue@epitech.eu>

---
+++ mobile/lib/app/theme/colors/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const ColorScheme triggoColorScheme = ColorScheme(
+  primary: Color(0xFF913AEE),
+  secondary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFF913AEE),
+  secondaryContainer: Color(0xFFF6F6F6),
+  surface: Color(0xFFFFFFFF), // Isn't set for now
+  error: Color(0xFFFFFFFF),
+  onPrimary: Color(0xFFFFFFFF),
+  onSecondary: Color(0xFFFFFFFF),
+  onPrimaryContainer: Color(0xFFFFFFFF),
+  onSecondaryContainer: Color(0xFFFFFFFF),
+  onSurface: Color(0xFFFFFFFF), // Isn't set for now
+  onError: Color(0xFFDA4141),
+  brightness: Brightness.light,
+);
+
+const Color textColor = Color(0xFF3E244A);
---
+++ mobile/lib/app/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:triggo/app/theme/colors/colors.dart';
+import 'package:triggo/app/theme/fonts/fonts.dart';
+
+final ThemeData triggoTheme = ThemeData(
+  colorScheme: triggoColorScheme,
+  textTheme: triggoTextTheme,
+);
