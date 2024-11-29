b3a24e1425eb239a9857f3a11c93f970262e80c7
sdragos1 <dragos.suceveanu@epitech.eu>
2024-11-29 23:01:51 +0100

    feat(web): add logout in auth mediator and repository

--- web/src/mediators/auth.mediator.ts
+++ web/src/mediators/auth.mediator.ts
@@ -1,14 +1,16 @@
-import {inject, Injectable, signal} from '@angular/core';
-import {AuthRepository} from '@repositories/auth';
-import {Observable, tap} from 'rxjs';
-import {TokensModel} from '@models/tokens.model';
-import {AuthUserModel} from '@models/auth-user.model';
+import { inject, Injectable, signal } from '@angular/core';
+import { AuthRepository } from '@repositories/auth';
+import { Observable, tap } from 'rxjs';
+import { TokensModel } from '@models/tokens.model';
+import { AuthUserModel } from '@models/auth-user.model';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthMediator {
   readonly #authRepository = inject(AuthRepository);
+  readonly #router = inject(Router);
 
   getTokens(): TokensModel {
     return this.#authRepository.getTokens();
@@ -22,21 +24,33 @@ export class AuthMediator {
     return this.#authRepository.getRefreshToken();
   }
 
-  register(email: string, password: string, firstName: string, lastName: string): Observable<TokensModel> {
-    return this.#authRepository.register({
-      email: email,
-      password: password,
-      firstName: firstName,
-      lastName: lastName
-    }).pipe(
-      tap({
-        next: tokens => this.#authRepository.storeTokens(tokens),
-        error: error => console.error('Failed to register user', error)
+  register(
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string
+  ): Observable<TokensModel> {
+    return this.#authRepository
+      .register({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
       })
-    );
+      .pipe(
+        tap({
+          next: tokens => this.#authRepository.storeTokens(tokens),
+          error: error => console.error('Failed to register user', error),
+        })
+      );
   }
 
   me(): Observable<AuthUserModel> {
     return this.#authRepository.me();
+  }
+
+  logout(): void {
+    this.#authRepository.removeTokens();
+    void this.#router.navigate(['/register']);
   }
 }
