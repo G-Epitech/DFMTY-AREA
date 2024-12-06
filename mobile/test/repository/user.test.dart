f65ba86230476ca13cd091ae581bbda99e5bca9d
thomaltarix <thomas.boue@epitech.eu>
2024-12-06 12:40:16 +0100

    feat(mobile): add not passing tests for the user repository

---
+++ mobile/test/repository/user.test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+import 'package:triggo/api/codes.dart';
+import 'package:triggo/repositories/credentials.repository.dart';
+import 'package:triggo/repositories/user.repository.dart';
+
+import '../api/call.test.mocks.dart';
+import '../api/mock/init.mock.dart';
+import 'credentials.test.mocks.dart';
+
+void userRepositoryTests() {
+  late MockClient mock;
+  late UserRepository repository;
+  late CredentialsRepository credentialsRepository;
+  late MockFlutterSecureStorage mockSecureStorage;
+
+  setUp(() {
+    mock = MockClient();
+    initMock(mock);
+
+    mockSecureStorage = MockFlutterSecureStorage();
+    credentialsRepository =
+        CredentialsRepository(secureStorage: mockSecureStorage);
+    repository = UserRepository(credentialsRepository: credentialsRepository);
+
+    when(mockSecureStorage.read(key: 'accessToken'))
+        .thenAnswer((_) async => 'dummy');
+
+    when(mock.get(
+      Uri.parse('/user'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer dummy'
+      },
+    )).thenAnswer((_) async => http.Response(
+          '{"id": "example", "firstName": "example", "lastName": "example", "email": "example@example.com",'
+          '"picture": "example"'
+          '}',
+          200,
+          headers: {'Content-Type': 'application/json'},
+        ));
+  });
+
+  group('UserRepository', () {
+    test('getUser success', () async {
+      final response = await repository.getUser();
+
+      expect(response.statusCode, equals(Codes.ok));
+      expect(response.data?.id, "example");
+      expect(response.data?.firstName, "example");
+      expect(response.data?.lastName, "example");
+      expect(response.data?.email, "example@example.com");
+      expect(response.data?.picture, "example");
+    });
+  });
+}
