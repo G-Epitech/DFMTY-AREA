b60fe13872572e5881bb25425fc5e7319a0076eb
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-08 16:57:34 +0100

    feat(web): add available integration card

--- web/src/app/features/integrations/components/integration-add-dialog/integration-add-dialog.component.ts
+++ web/src/app/features/integrations/components/integration-add-dialog/integration-add-dialog.component.ts
@@ -1,22 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TrDialogImports } from '@triggo-ui/dialog';
 import { BrnDialogImports } from '@spartan-ng/ui-dialog-brain';
-import { LabelDirective } from '@triggo-ui/label';
-import { TrInputDirective } from '@triggo-ui/input';
 import { TrButtonDirective } from '@triggo-ui/button';
+import {
+  IntegrationAvailableCardComponent,
+  IntegrationAvailableCardProps,
+} from '@features/integrations/components/integration-available-card/integration-available-card.component';
 
 @Component({
   selector: 'tr-integration-add-dialog',
   imports: [
     TrDialogImports,
     BrnDialogImports,
-    LabelDirective,
-    TrInputDirective,
     TrButtonDirective,
+    IntegrationAvailableCardComponent,
   ],
   templateUrl: './integration-add-dialog.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class IntegrationAddDialogComponent {}
+export class IntegrationAddDialogComponent {
+  readonly availableIntegrations: IntegrationAvailableCardProps[] = [
+    {
+      logoAssetName: 'icons/discord_logo.svg',
+      name: 'Discord',
+      description:
+        'Connect your Discord server to Triggo and receive notifications about your projects.',
+    },
+  ];
+}
---
+++ web/src/app/features/integrations/components/integration-available-card/integration-available-card.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+export interface IntegrationAvailableCardProps {
+  logoAssetName: string;
+  name: string;
+  description: string;
+}
+
+@Component({
+  selector: 'tr-integration-available-card',
+  imports: [NgOptimizedImage],
+  templateUrl: './integration-available-card.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class IntegrationAvailableCardComponent {
+  props = input.required<IntegrationAvailableCardProps>();
+}
