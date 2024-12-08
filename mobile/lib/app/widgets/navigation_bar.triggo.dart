e1331ef899ccb62b6ca041f000895534b160a99c
thomaltarix <thomas.boue@epitech.eu>
2024-12-08 15:11:14 +0100

    refactor(mobile): make navbar button selected by default

--- mobile/lib/app/widgets/navigation_bar.triggo.dart
+++ mobile/lib/app/widgets/navigation_bar.triggo.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:triggo/app/features/card/bloc/card_bloc.dart';
 import 'package:triggo/app/routes/routes_names.dart';
 import 'package:triggo/app/theme/colors/colors.dart';
+
+final ValueNotifier<String?> currentRouteNotifier =
+    ValueNotifier<String?>(RoutesNames.home);
 
 class TriggoNavigationBar extends StatelessWidget {
   const TriggoNavigationBar({super.key});
@@ -40,46 +41,38 @@ class _TriggoNavigationBarItem {
       {required this.iconAsset, required this.routeName});
 
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CardBloc(),
-      child: Builder(
-        builder: (context) {
-          return Expanded(
-            child: GestureDetector(
-              onTap: () {
-                if (ModalRoute.of(context)?.settings.name != routeName) {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                      routeName, (route) => route.settings.name == routeName);
-                }
-              },
-              onTapDown: (_) => context.read<CardBloc>().add(CardEvent.press),
-              onTapUp: (_) => context.read<CardBloc>().add(CardEvent.release),
-              onTapCancel: () =>
-                  context.read<CardBloc>().add(CardEvent.release),
-              child: BlocBuilder<CardBloc, bool>(
-                builder: (context, isPressed) {
-                  return Card(
-                    color: isPressed ? lightContainer : Colors.grey[100],
-                    child: SizedBox(
-                      height: 80,
-                      child: Center(
-                        child: SvgPicture.asset(
-                          iconAsset,
-                          color: isPressed
-                              ? Theme.of(context).colorScheme.primary
-                              : Theme.of(context).textTheme.titleMedium?.color,
-                          width: 32,
-                          height: 32,
-                        ),
-                      ),
-                    ),
-                  );
-                },
+    return ValueListenableBuilder<String?>(
+      valueListenable: currentRouteNotifier,
+      builder: (context, currentRoute, child) {
+        final bool isSelected = currentRoute == routeName;
+        return Expanded(
+          child: GestureDetector(
+            onTap: () {
+              if (currentRoute != routeName) {
+                currentRouteNotifier.value = routeName;
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    routeName, (route) => route.settings.name == routeName);
+              }
+            },
+            child: Card(
+              color: isSelected ? lightContainer : Colors.grey[100],
+              child: SizedBox(
+                height: 80,
+                child: Center(
+                  child: SvgPicture.asset(
+                    iconAsset,
+                    color: isSelected
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).textTheme.titleMedium?.color,
+                    width: 32,
+                    height: 32,
+                  ),
+                ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
