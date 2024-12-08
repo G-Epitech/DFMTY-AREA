6a05e2580f91ce5e541f803a8046f1f9153244aa
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-09 00:30:01 +0100

    feat(web): pimp out the discord oauth2 page

--- web/src/repositories/integrations/discord.repository.ts
+++ web/src/repositories/integrations/discord.repository.ts
@@ -1,6 +1,6 @@
 import { Inject, inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { DiscordUriResponseDTO } from '@repositories/integrations/dto/discord';
 import { DiscordRequestDTO } from '@repositories/integrations/dto/discord/discord.dto';
 
@@ -12,23 +12,14 @@ export class DiscordRepository {
 
   constructor(@Inject('BASE_URL') private baseUrl: string) {}
 
-  getUri() {
+  getUri(): Observable<string> {
     const url = `${this.baseUrl}/integrations/discord/uri`;
     const response = this.#httpClient.post<DiscordUriResponseDTO>(url, {});
-    return response.pipe(
-      map(
-        res => res.uri
-      )
-    );
+    return response.pipe(map(res => res.uri));
   }
 
-  link(dto: DiscordRequestDTO) {
+  link(dto: DiscordRequestDTO): Observable<void> {
     const url = `${this.baseUrl}/integrations/discord`;
-    const response = this.#httpClient.post<null>(url, dto);
-    return response.pipe(
-      map(
-        res => true
-      )
-    );
+    return this.#httpClient.post<void>(url, dto);
   }
 }
