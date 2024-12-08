aec0b9ae0e75f9209ad2bdc1622f8c5eee7d7df6
Yann <yannmasson17@gmail.com>
2024-12-08 20:17:09 +0100

    feat(web): update oauth2 discord with each steps
    Those steps are: get URI path, redirect, link integration

--- web/src/app/features/oauth2/discord/discord.oauth2.page.ts
+++ web/src/app/features/oauth2/discord/discord.oauth2.page.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { DiscordRepository } from '@repositories/integrations';
 
 @Component({
   selector: 'tr-oauth2-discord',
@@ -11,20 +12,44 @@ import { filter } from 'rxjs/operators';
   standalone: true,
 })
 export class DiscordOAuth2PageComponent implements OnInit {
-  accessToken: string | null = null;
-  order: string | null = null;
+  readonly #discordRepository = inject(DiscordRepository);
 
-  constructor(private route: ActivatedRoute) { }
+  accessToken: string | null = null;
+  code: string | null = null;
+  state: string | null = null;
+
+  uri: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.queryParams
-      .pipe(filter(params => params['accessToken']))
+      .pipe(filter(params => params['accessToken'] || params['code'] || params['state']))
       .subscribe(params => {
-        console.log(params);
+        console.log("Query params received:", params);
 
         this.accessToken = params['accessToken'];
+        this.code = params['code'];
+        this.state = params['state'];
 
-        console.log(this.accessToken);
+        console.log("Access token:", this.accessToken);
+
+        if (this.accessToken) {
+          localStorage.setItem('accessToken', this.accessToken);
+        }
       });
+
+    console.log("DiscordOAuth2PageComponent initialized");
+
+    if (this.code && this.state) {
+      this.#discordRepository.link({ code: this.code, state: this.state }).subscribe();
+    } else {
+      this.#discordRepository.getUri().subscribe(uri => {
+        console.log("Discord URI received:", uri);
+        this.uri = uri;
+        window.location.href = uri;
+      });
+    }
+
   }
 }
