c28496f7390005b452d32ceffdf5550a5f72d1af
thomaltarix <thomas.boue@epitech.eu>
2024-12-10 00:22:51 +0100

    feat(mobile): add prebuild for the automation page

--- mobile/lib/app/features/automations/view/automation_page.view.dart
+++ mobile/lib/app/features/automations/view/automation_page.view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:triggo/app/features/integrations/widgets/integration.widget.dart';
 import 'package:triggo/app/routes/routes_names.dart';
 import 'package:triggo/app/widgets/button.triggo.dart';
 import 'package:triggo/app/widgets/scaffold.triggo.dart';
@@ -38,6 +39,7 @@ class _AutomationContainer extends StatelessWidget {
             ),
           ],
         ),
+        Expanded(child: _AutomationList()),
       ],
     );
   }
@@ -53,6 +55,98 @@ class _AutomationCreationButton extends StatelessWidget {
       onPressed: () {
         Navigator.pushNamed(context, RoutesNames.createAutomation);
       },
+    );
+  }
+}
+
+class _AutomationList extends StatelessWidget {
+  const _AutomationList();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20, // Need to be changed to the automation length
+      itemBuilder: (context, index) {
+        return _AutomationListItem(); // Need to be changed to AutomationCard
+      },
+    );
+  }
+}
+
+class _AutomationListItem extends StatelessWidget {
+  const _AutomationListItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return IntegrationCard(
+        customWidget: Row(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.network(
+                "https://via.placeholder.com/150",
+                width: 60,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: -5,
+              right: -5,
+              child: _ActivityIcon(state: "active"),
+            )
+          ],
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("Reply \"feur\" to \"Quoi\"",
+                      style: Theme.of(context).textTheme.labelLarge),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  Text(
+                    "Last update: Monday 18:24",
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
+
+class _ActivityIcon extends StatelessWidget {
+  final String state;
+  final Color color;
+  _ActivityIcon({required this.state}) : color = _getColor(state);
+
+  static Color _getColor(String state) {
+    switch (state) {
+      case "active":
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 10,
+      backgroundColor: color,
     );
   }
 }
