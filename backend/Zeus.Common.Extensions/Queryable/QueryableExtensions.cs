4647ea0ec2e27eb9e74bfdae1589deb53bd403a3
Flavien Chenu <flavienchenu@hotmail.com>
2024-12-09 01:20:35 +0100

    fix(backend): fix pagination total number of pages

--- backend/Zeus.Common.Extensions/Queryable/QueryableExtensions.cs
+++ backend/Zeus.Common.Extensions/Queryable/QueryableExtensions.cs
@@ -10,13 +10,16 @@ public static class QueryableExtensions
             throw new ArgumentOutOfRangeException(nameof(query.Limit), "Limit must be greater than 0");
     }
 
+    private static int GetTotalPages(int totalItems, int limit)
+    {
+        var total = (int)Math.Ceiling(totalItems / (double)limit);
+
+        return total == 0 ? 1 : total;
+    }
+
     public static Page<T> Paginate<T>(this IQueryable<T> queryable, int index, int limit)
     {
-        return queryable.Paginate(new PageQuery
-        {
-            Index = index,
-            Limit = limit
-        });
+        return queryable.Paginate(new PageQuery { Index = index, Limit = limit });
     }
 
     public static Page<T> Paginate<T>(this IQueryable<T> queryable, PageQuery query)
@@ -28,20 +31,16 @@ public static class QueryableExtensions
             .Take(query.Limit);
 
         var totalItems = queryable.Count();
-        var totalPages = (int) Math.Ceiling(totalItems / (double) query.Limit) + 1;
-        
+        var totalPages = GetTotalPages(totalItems, query.Limit);
+
         return new Page<T>(query.Index, query.Limit, totalPages, totalItems, items.ToList());
     }
 
     public static async Task<Page<T>> PaginateAsync<T>(this IAsyncQueryable<T> queryable, int index, int size, CancellationToken cancellationToken = default)
     {
-        return await queryable.PaginateAsync(new PageQuery
-        {
-            Index = index,
-            Limit = size
-        }, cancellationToken);
+        return await queryable.PaginateAsync(new PageQuery { Index = index, Limit = size }, cancellationToken);
     }
-    
+
     public static async Task<Page<T>> PaginateAsync<T>(this IAsyncQueryable<T> queryable, PageQuery query, CancellationToken cancellationToken = default)
     {
         ValidatePageQuery(query);
@@ -51,9 +50,9 @@ public static class QueryableExtensions
             .Take(query.Limit);
 
         var totalItems = await queryable.CountAsync(cancellationToken);
-        var totalPages = (int) Math.Ceiling(totalItems / (double)query.Limit) + 1;
+        var totalPages = GetTotalPages(totalItems, query.Limit);
         var itemsList = await items.ToListAsync(cancellationToken);
-        
+
         return new Page<T>(query.Index, query.Limit, totalPages, totalItems, itemsList);
     }
 }
