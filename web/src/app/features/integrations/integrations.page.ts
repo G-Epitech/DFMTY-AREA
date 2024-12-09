ff07e89848db50faadf05c153f7f3f2efdc55468
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-09 21:00:23 +0100

    feat(web): add pagination to the integrations listing

--- web/src/app/features/integrations/integrations.page.ts
+++ web/src/app/features/integrations/integrations.page.ts
@@ -1,16 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  effect,
   inject,
-  OnInit,
   signal,
 } from '@angular/core';
-import {
-  IntegrationModel,
-  IntegrationTypeEnum,
-  IntegrationDiscordProps,
-} from '@models/integration';
-import { Observable, of, tap } from 'rxjs';
+import { IntegrationModel } from '@models/integration';
+import { BehaviorSubject, Observable, of, switchMap, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { IntegrationLinkedCardComponent } from '@features/integrations/components/integration-linked/integration-linked-card.component';
 import { IntegrationAddDialogComponent } from '@features/integrations/components/integration-add-dialog/integration-add-dialog.component';
@@ -30,8 +26,6 @@ import { TrSkeletonComponent } from '@triggo-ui/skeleton';
     IntegrationLinkedCardComponent,
     IntegrationAddDialogComponent,
     PaginationComponent,
-    TrInputDirective,
-    TrSpinnerComponent,
     TrSkeletonComponent,
     TrInputSearchComponent,
   ],
@@ -42,28 +36,43 @@ import { TrSkeletonComponent } from '@triggo-ui/skeleton';
 })
 export class IntegrationsPageComponent {
   readonly #usersMediator = inject(UsersMediator);
-  readonly #store = inject(AuthStore);
 
   pageOptions = signal<PageOptions>({
-    page: 1,
+    page: 0,
     size: 5,
   });
   totalPages = signal<number>(3);
   loading = signal<boolean>(true);
 
-  readonly integrations: Observable<PageModel<IntegrationModel>> | null =
-    this.#store.user()
-      ? this.#usersMediator
-          .getIntegrations(this.#store.user()!.id, this.pageOptions())
-          .pipe(
-            tap(page => {
-              this.totalPages.set(page.totalPages);
-              this.loading.set(false);
-            })
+  #pageOptionsSubject = new BehaviorSubject<PageOptions>(this.pageOptions());
+
+  readonly integrations: Observable<PageModel<IntegrationModel>> =
+    this.#usersMediator.me().pipe(
+      switchMap(user =>
+        this.#pageOptionsSubject.pipe(
+          switchMap(pageOptions =>
+            this.#usersMediator.getIntegrations(user.id, pageOptions)
           )
-      : null;
+        )
+      ),
+      tap(page => {
+        this.totalPages.set(page.totalPages);
+        this.loading.set(false);
+      })
+    );
+
+  constructor() {
+    effect(() => {
+      const currentPageOptions = this.pageOptions();
+      this.#pageOptionsSubject.next(currentPageOptions);
+    });
+  }
 
   pageChanged(page: number): void {
-    this.pageOptions.update(options => ({ ...options, page }));
+    this.loading.set(true);
+    this.pageOptions.update(options => ({
+      size: options.size,
+      page: page - 1,
+    }));
   }
 }
