eb01d5a14814c5f7b012465f2cbf22064c881bb2
TekMath <matheo.coquet@epitech.eu>
2024-12-09 23:33:48 +0100

    feat(backend): add trigger handler

---
+++ backend/Zeus.Daemon.Application/Abstracts/TriggerHandler.cs
@@ -0,0 +1,32 @@
+using Zeus.Daemon.Application.Interfaces;
+using Zeus.Daemon.Domain.Automation;
+using Zeus.Daemon.Domain.Automation.AutomationAggregate.Entities;
+
+namespace Zeus.Daemon.Application.Abstracts;
+
+public abstract class TriggerHandler : ITriggerHandler
+{
+    public abstract Task HandleAsync(AutomationExecutionContext context, CancellationToken cancellationToken);
+
+    public abstract Task ListenAsync(AutomationExecutionContext context, CancellationToken cancellationToken);
+
+    public Task ExecuteAsync(AutomationExecutionContext context, Dictionary<string, string> facts,
+        CancellationToken cancellationToken)
+    {
+        var actions = context.Automation.Actions;
+
+        foreach (AutomationAction action in actions)
+        {
+            if (action.Providers.Count <= 0)
+            {
+                continue;
+            }
+
+            var integration = context.Integrations.FirstOrDefault(i => i.Id == action.Providers[0]);
+
+            // Execute the action with the integration and facts
+        }
+
+        return Task.CompletedTask;
+    }
+}
