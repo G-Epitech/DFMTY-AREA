c7a30d34061c58a7c7a5fea574c979543038ade9
Yann <yannmasson17@gmail.com>
2024-12-10 20:04:18 +0100

    feat(mobile): add getAutomationManifest method to automation repository

---
+++ mobile/lib/repositories/automation.repository.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'package:triggo/api/call.dart';
+import 'package:triggo/api/response.dart';
+import 'package:triggo/repositories/credentials.repository.dart';
+import 'package:triggo/repositories/dtos/automation.dtos.dart';
+
+class AutomationRepository {
+  final http.Client? client;
+  final CredentialsRepository credentialsRepository;
+
+  AutomationRepository({this.client, required this.credentialsRepository});
+
+  Future<Response<OutGetAutomationManifestDTO>> getAutomationManifest(
+      {int page = 0, int size = 10}) async {
+    final accessToken = await credentialsRepository.getAccessToken();
+    final response = await call(
+      method: 'GET',
+      endpoint: '/user/integrations/?page=$page&size=$size',
+      headers: {'Authorization': 'Bearer $accessToken'},
+      client: client,
+    );
+
+    return Response<OutGetAutomationManifestDTO>(
+      statusCode: response.statusCode,
+      message: response.message,
+      headers: response.headers,
+      data: response.data != null
+          ? OutGetAutomationManifestDTO.fromJson(response.data)
+          : null,
+      errors: response.errors,
+    );
+  }
+}
