46dbe039f98a41df260ad0aa1e011a3454873bda
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-10 13:28:15 +0100

    feat(web): add base for the static automations page

--- web/src/app/features/automations/automations.page.ts
+++ web/src/app/features/automations/automations.page.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TrButtonDirective } from '@triggo-ui/button';
+import { PaginationComponent } from '@app/components';
+import { TrInputSearchComponent } from '@triggo-ui/input';
 
 @Component({
   selector: 'tr-automations',
-  imports: [],
+  imports: [TrButtonDirective, PaginationComponent, TrInputSearchComponent],
   templateUrl: './automations.page.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class AutomationsPageComponent {}
+export class AutomationsPageComponent {
+  totalPages = signal<number>(21);
+}
