c16fff1feb5858361f57568a026be4f86c1cbb56
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-10 14:16:21 +0100

    feat(web): add format date pipe

--- web/src/app/features/automations/components/automation-card/automation-card.component.ts
+++ web/src/app/features/automations/components/automation-card/automation-card.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { AutomationModel } from '@models/automation.model';
 import { NgIcon } from '@ng-icons/core';
 import { NgClass } from '@angular/common';
+import { FormatDatePipe } from '@app/pipes';
 
 @Component({
   selector: 'tr-automation-card',
-  imports: [NgIcon, NgClass],
+  imports: [NgIcon, NgClass, FormatDatePipe],
   templateUrl: './automation-card.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- web/src/app/pipes/format-date.pipe.ts
+++ web/src/app/pipes/format-date.pipe.ts
@@ -1,12 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'formatDate'
+  name: 'formatDate',
 })
 export class FormatDatePipe implements PipeTransform {
+  transform(value: Date | string | number): string {
+    if (!value) return 'Invalid date';
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const date = new Date(value);
+    const now = new Date();
+
+    const isToday = date.toDateString() === now.toDateString();
+
+    const yesterday = new Date(now);
+    yesterday.setDate(now.getDate() - 1);
+    const isYesterday = date.toDateString() === yesterday.toDateString();
+
+    let dayLabel = '';
+    if (isToday) {
+      dayLabel = 'Today';
+    } else if (isYesterday) {
+      dayLabel = 'Yesterday';
+    } else {
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      dayLabel = days[date.getDay()];
+    }
+
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const time = `${hours}:${minutes}`;
+
+    return `${dayLabel} ${time}`;
   }
-
 }
