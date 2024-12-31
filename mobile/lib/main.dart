17f69e73b84a386a5ba13adedc35238f4916f6ae
Yann <yannmasson17@gmail.com>
2024-12-31 17:32:00 +0100

    feat(mobile): set orientations to only portrait (up or down)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:triggo/app/app.dart';
 
 void main() {
   runApp(const MyApp());
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 }
