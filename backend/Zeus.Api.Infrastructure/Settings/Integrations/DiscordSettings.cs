f30e1d42960e1b0d03597023c6b77ce97cae8b73
TekMath <matheo.coquet@epitech.eu>
2025-01-07 15:03:59 +0100

    feat(api): add bot token to web api settings

--- backend/Zeus.Api.Infrastructure/Settings/Integrations/DiscordSettings.cs
+++ backend/Zeus.Api.Infrastructure/Settings/Integrations/DiscordSettings.cs
@@ -6,6 +6,7 @@ public class DiscordSettings
     public string OAuth2Endpoint { get; init; } = null!;
     public string ClientId { get; init; } = null!;
     public string ClientSecret { get; init; } = null!;
+    public string BotToken { get; init; } = null!;
     public string RedirectUrl { get; init; } = null!;
     public List<string> Scope { get; init; } = null!;
 }
