4bdab760b8763bfe278f5f1dff05e4d8945b56a4
TekMath <matheo.coquet@epitech.eu>
2025-01-07 16:39:24 +0100

    feat(api): add get discord guild channels endpoint

--- backend/Zeus.Api.Presentation.Web/Controllers/Integrations/Discord/DiscordPropsController.cs
+++ backend/Zeus.Api.Presentation.Web/Controllers/Integrations/Discord/DiscordPropsController.cs
@@ -4,6 +4,7 @@ using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 
+using Zeus.Api.Application.Integrations.Query.Discord.GetDiscordGuildChannels;
 using Zeus.Api.Application.Integrations.Query.Discord.GetDiscordUserGuilds;
 using Zeus.Api.Infrastructure.Authentication.Context;
 using Zeus.Api.Presentation.Web.Contracts.Integrations.Discord;
@@ -38,6 +39,23 @@ public class DiscordPropsController : ApiController
 
         return userGuildsResult.Match(
             result => Ok(result.Select(guild => _mapper.Map<GetDiscordGuildResponse>(guild))),
+            Problem);
+    }
+    
+    [HttpGet("guilds/{guildId}/channels", Name = "GetDiscordGuildChannels")]
+    [ProducesResponseType<List<GetDiscordGuildChannelResponse>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetDiscordGuildChannels(string guildId, Guid integrationId)
+    {
+        var authUser = _authUserContext.User;
+        if (authUser is null)
+        {
+            return Unauthorized();
+        }
+
+        var guildChannels = await _sender.Send(new GetDiscordGuildChannelsQuery(authUser.Id, integrationId, guildId));
+
+        return guildChannels.Match(
+            result => Ok(result.Select(guild => _mapper.Map<GetDiscordGuildChannelResponse>(guild))),
             Problem);
     }
 }
