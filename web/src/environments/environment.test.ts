676237d791b3ecce85d1de2241f5dc93a0547379
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-09 21:51:28 +0100

    chore(web): add test environment

---
+++ web/src/environments/environment.test.ts
@@ -0,0 +1,7 @@
+import { EnvironmentInterface } from '@environments/environment.interface';
+
+export const environment : EnvironmentInterface = {
+  production: false,
+  apiUrl: 'https://placeholder.com',
+  integrationSettingsClientId: 'clientid',
+}
