e681d41217c235bd90ad48967996814cdf7270af
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-09 18:14:47 +0100

    feat(web): add listing of discord guilds

--- web/src/app/features/integrations/discord/integration-linked-discord/integration-linked-discord.component.ts
+++ web/src/app/features/integrations/discord/integration-linked-discord/integration-linked-discord.component.ts
@@ -1,16 +1,40 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { IntegrationDiscordProps } from '@models/integration';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { IntegrationDiscordProps, IntegrationModel } from '@models/integration';
 import { NgOptimizedImage } from '@angular/common';
 import { ManageGuildDialogComponent } from '@features/integrations/discord/manage-guild-dialog/manage-guild-dialog.component';
+import { BrnDialogTriggerDirective } from '@spartan-ng/ui-dialog-brain';
+import { TrButtonDirective } from '@triggo-ui/button';
+import { TrDialogService } from '@triggo-ui/dialog';
 
 @Component({
   selector: 'tr-integration-linked-discord',
-  imports: [NgOptimizedImage, ManageGuildDialogComponent],
+  imports: [
+    NgOptimizedImage,
+    ManageGuildDialogComponent,
+    BrnDialogTriggerDirective,
+    TrButtonDirective,
+  ],
   templateUrl: './integration-linked-discord.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
 export class IntegrationLinkedDiscordComponent {
-  props = input.required<IntegrationDiscordProps>();
+  readonly #dialogService = inject(TrDialogService);
+
+  integration = input.required<IntegrationModel>();
+
+  openModal() {
+    this.#dialogService.open(ManageGuildDialogComponent, {
+      context: {
+        integrationId: this.integration().id,
+      },
+      contentClass: 'min-w-[800px] min-h-[600px]',
+    });
+  }
 }
--- web/src/app/features/integrations/discord/manage-guild-dialog/manage-guild-dialog.component.ts
+++ web/src/app/features/integrations/discord/manage-guild-dialog/manage-guild-dialog.component.ts
@@ -1,18 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BrnDialogImports } from '@spartan-ng/ui-dialog-brain';
-import { NgClass, NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+import {
+  BrnDialogImports,
+  BrnDialogRef,
+  injectBrnDialogContext,
+} from '@spartan-ng/ui-dialog-brain';
+import { AsyncPipe, NgClass, NgOptimizedImage } from '@angular/common';
 import { TrDialogImports } from '@triggo-ui/dialog';
 import { NgIcon } from '@ng-icons/core';
 import { TrButtonDirective } from '@triggo-ui/button';
-import { TrInputDirective, TrInputSearchComponent } from '@triggo-ui/input';
-
-interface GuildProps {
-  id: string;
-  name: string;
-  iconUri: string;
-  approximateMemberCount: number;
-  linked: boolean;
-}
+import { TrInputSearchComponent } from '@triggo-ui/input';
+import { IntegrationsMediator } from '@mediators/integrations.mediator';
+import { Observable } from 'rxjs';
+import { DiscordGuildModel } from '@models/integration';
 
 @Component({
   selector: 'tr-manage-guild-dialog',
@@ -23,79 +28,27 @@ interface GuildProps {
     NgIcon,
     NgClass,
     TrButtonDirective,
-    TrInputDirective,
     TrInputSearchComponent,
+    AsyncPipe,
   ],
   templateUrl: './manage-guild-dialog.component.html',
   standalone: true,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ManageGuildDialogComponent {
-  readonly guilds: GuildProps[] = [
-    {
-      id: '1',
-      name: "Programmer's Hangout",
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 500,
-      linked: true,
-    },
-    {
-      id: '2',
-      name: 'Angular Devs',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 21300,
-      linked: false,
-    },
-    {
-      id: '3',
-      name: 'Triggo Lovers',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 1232100,
-      linked: false,
-    },
-    {
-      id: '4',
-      name: 'Mario Kart Fans',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 100,
-      linked: false,
-    },
-    {
-      id: '5',
-      name: 'Mario Kart Fans',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 100,
-      linked: false,
-    },
-    {
-      id: '6',
-      name: 'Mario Kart Fans',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 100,
-      linked: false,
-    },
-    {
-      id: '7',
-      name: 'Mario Kart Fans',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 100,
-      linked: false,
-    },
-    {
-      id: '8',
-      name: 'Mario Kart Fans',
-      iconUri:
-        'https://play.nintendo.com/images/profile-mk-kamek.7bf2a8f2.aead314d58b63e27.png',
-      approximateMemberCount: 100,
-      linked: false,
-    },
-  ];
+export class ManageGuildDialogComponent implements OnInit {
+  readonly #integrationMediator = inject(IntegrationsMediator);
+  readonly #dialogContext = injectBrnDialogContext<{
+    integrationId: string;
+  }>();
+
+  protected readonly integrationId = this.#dialogContext.integrationId;
+
+  guilds$: Observable<DiscordGuildModel[]> | undefined;
+
+  ngOnInit() {
+    if (this.integrationId) {
+      this.guilds$ = this.#integrationMediator.getDiscordGuilds(this.integrationId);
+    }
+  }
 }
