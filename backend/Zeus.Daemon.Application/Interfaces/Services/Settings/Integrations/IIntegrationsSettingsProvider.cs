a77f7890931f9ba07907899aad4beb4e51285202
TekMath <matheo.coquet@epitech.eu>
2025-01-10 11:31:36 +0100

    feat(backend): add notion integration configuration

--- backend/Zeus.Daemon.Application/Interfaces/Services/Settings/Integrations/IIntegrationsSettingsProvider.cs
+++ backend/Zeus.Daemon.Application/Interfaces/Services/Settings/Integrations/IIntegrationsSettingsProvider.cs
@@ -3,4 +3,5 @@ namespace Zeus.Daemon.Application.Interfaces.Services.Settings.Integrations;
 public interface IIntegrationsSettingsProvider
 {
     public IDiscordSettingsProvider Discord { get; }
+    public INotionSettingsProvider Notion { get; }
 }
---
+++ backend/Zeus.Daemon.Application/Interfaces/Services/Settings/Integrations/INotionSettingsProvider.cs
@@ -0,0 +1,6 @@
+namespace Zeus.Daemon.Application.Interfaces.Services.Settings.Integrations;
+
+public interface INotionSettingsProvider
+{
+    public string ApiEndpoint { get; }
+}
