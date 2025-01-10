d30273267ce74f2fb8a22f19d77bd8a2e29031aa
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-10 17:06:59 +0100

    feat(web): add general layout for the settings page

--- web/src/app/features/settings/settings.page.ts
+++ web/src/app/features/settings/settings.page.ts
@@ -2,10 +2,17 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthMediator } from '@mediators/auth.mediator';
 import { AuthStore } from '@app/store';
 import { TrButtonDirective } from '@triggo-ui/button';
+import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
+import { TrSkeletonComponent } from '@triggo-ui/skeleton';
 
 @Component({
   selector: 'tr-settings',
-  imports: [TrButtonDirective],
+  imports: [
+    TrButtonDirective,
+    NgOptimizedImage,
+    TitleCasePipe,
+    TrSkeletonComponent,
+  ],
   templateUrl: './settings.page.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,6 +21,8 @@ import { TrButtonDirective } from '@triggo-ui/button';
 export class SettingsPageComponent {
   readonly #authMediator = inject(AuthMediator);
   readonly #authStore = inject(AuthStore);
+
+  readonly user = this.#authStore.getUser;
 
   logout() {
     this.#authMediator.logout();
