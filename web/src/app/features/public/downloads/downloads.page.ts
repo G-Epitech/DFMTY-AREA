230678ed5d513fbd600f3e4ffe1a61e79427b274
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-13 19:42:31 +0100

    feat(web): add base layout for downloads page

--- web/src/app/features/public/downloads/downloads.page.ts
+++ web/src/app/features/public/downloads/downloads.page.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
+import { NgOptimizedImage } from '@angular/common';
+import { TrButtonDirective } from '@triggo-ui/button';
 
 @Component({
   selector: 'tr-downloads',
-  imports: [],
+  imports: [NgIcon, NgOptimizedImage, TrButtonDirective],
   templateUrl: './downloads.page.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
