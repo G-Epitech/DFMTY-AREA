d801d7c695151cb3d28d90e199e87aae075f8db4
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-13 17:30:44 +0100

    feat(web): add the base for the landing page

--- web/src/app/features/public/landing/landing.page.ts
+++ web/src/app/features/public/landing/landing.page.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TrButtonDirective } from '@triggo-ui/button';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tr-landing',
-  imports: [],
+  imports: [TrButtonDirective, NgOptimizedImage, RouterLink],
   templateUrl: './landing.page.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
