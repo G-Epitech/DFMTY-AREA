64c6bdd1d3fa339e2962279e4a40921129d9d93e
Flavien Chenu <flavienchenu@hotmail.com>
2025-01-14 19:59:27 +0100

    feat(backend): add /auth/refresh endpoint

---
+++ backend/Zeus.Api.Application/Authentication/Queries/RefreshTokens/RefreshTokensQueryResult.cs
@@ -0,0 +1,6 @@
+﻿using Zeus.Common.Domain.Authentication.Common;
+
+namespace Zeus.Api.Application.Authentication.Queries.RefreshTokens;
+
+public record RefreshTokensQueryResult(AccessToken AccessToken,
+    RefreshToken RefreshToken);
