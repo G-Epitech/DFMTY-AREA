450e168bf965e63bb4a7dda7f2bf81a4296fea6c
thomaltarix <thomas.boue@epitech.eu>
2025-01-15 15:53:49 +0100

    fix(mobile): Fix build on LeagueOfLegends and OpenAI pages

--- mobile/lib/app/features/integration/models/models.dart
+++ mobile/lib/app/features/integration/models/models.dart
@@ -1,4 +1,4 @@
-export 'openAI/admin_api_token.dart';
-export 'openAI/api_token.dart';
 export 'leagueOfLegends/game_name.dart';
 export 'leagueOfLegends/tag_line.dart';
+export 'openAI/admin_api_token.dart';
+export 'openAI/api_token.dart';
--- mobile/lib/app/features/integration/view/integrations/leagueOfLegends.view.dart
+++ mobile/lib/app/features/integration/view/integrations/leagueOfLegends.view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:triggo/app/features/integration/bloc/integrations/leagueOfLegends/league_of_legends_bloc.dart';
 import 'package:triggo/app/features/integration/widgets/integrations/league_of_legends_form.widget.dart';
-import 'package:triggo/mediator/integrations/leagueOfLegends.mediator.dart';
+import 'package:triggo/mediator/integration.mediator.dart';
 
 class LeagueOfLegendsIntegrationView extends StatelessWidget {
   const LeagueOfLegendsIntegrationView({super.key});
@@ -22,7 +22,8 @@ class LeagueOfLegendsIntegrationView extends StatelessWidget {
         padding: const EdgeInsets.all(12),
         child: BlocProvider(
           create: (context) => LeagueOfLegendsIntegrationBloc(
-            leagueOfLegends: context.read<LeagueOfLegendsMediator>(),
+            leagueOfLegends:
+                context.read<IntegrationMediator>().leagueOfLegends,
           ),
           child: const LeagueOfLegendsIntegrationForm(),
         ),
