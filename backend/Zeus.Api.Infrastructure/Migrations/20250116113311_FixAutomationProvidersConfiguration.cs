59d2bd8284baccbe34f88fac1415521d6cfdf373
Flavien Chenu <flavienchenu@hotmail.com>
2025-01-16 12:52:11 +0100

    fix(backend): bad automation providers configuration

---
+++ backend/Zeus.Api.Infrastructure/Migrations/20250116113311_FixAutomationProvidersConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zeus.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAutomationProvidersConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AutomationTriggerProviders",
+                table: "AutomationTriggerProviders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AutomationTriggerProviders",
+                table: "AutomationTriggerProviders",
+                columns: new[] { "TriggerId", "ProviderId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AutomationTriggerProviders",
+                table: "AutomationTriggerProviders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AutomationTriggerProviders",
+                table: "AutomationTriggerProviders",
+                column: "TriggerId");
+        }
+    }
+}
