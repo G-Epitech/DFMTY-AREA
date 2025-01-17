85f0ac0e070e0cd9704827bf3dfc41fe2d11c2cf
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-17 15:15:03 +0100

    feat(web): add tabs component

--- web/src/app/features/automations/workspace/components/automation-parameter-edit/automation-parameter-edit.component.ts
+++ web/src/app/features/automations/workspace/components/automation-parameter-edit/automation-parameter-edit.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { PascalToPhrasePipe } from '@app/pipes';
 import { AutomationParameterType } from '@models/automation';
 import { NgIcon } from '@ng-icons/core';
+import { TrTabsImports } from '@triggo-ui/tabs';
 
 @Component({
   standalone: true,
   selector: 'tr-automation-parameter-edit',
-  imports: [PascalToPhrasePipe, NgIcon],
+  imports: [PascalToPhrasePipe, NgIcon, TrTabsImports],
   templateUrl: './automation-parameter-edit.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
