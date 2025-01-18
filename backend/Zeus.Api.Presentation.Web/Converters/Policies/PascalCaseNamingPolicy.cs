6dc48de063cf0ca01de36182d1ef7707ed1508d0
Flavien Chenu <flavienchenu@hotmail.com>
2025-01-18 09:53:11 +0100

    fix(backend): set all providers settings identifiers to PascalCase naming

---
+++ backend/Zeus.Api.Presentation.Web/Converters/Policies/PascalCaseNamingPolicy.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace Zeus.Api.Presentation.Web.Converters.Policies;
+
+public class PascalCaseNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            return name;
+
+        return char.ToUpper(name[0]) + name[1..];
+    }
+}
