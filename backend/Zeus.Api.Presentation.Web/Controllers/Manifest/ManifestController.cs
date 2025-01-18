25d8a2a7b42bfbd33ab7cd85b779f9b1e291b527
Flavien Chenu <flavienchenu@hotmail.com>
2025-01-18 14:44:57 +0100

    feat(backend): add API manifest

---
+++ backend/Zeus.Api.Presentation.Web/Controllers/Manifest/ManifestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Zeus.Api.Presentation.Web.Manifest;
+
+namespace Zeus.Api.Presentation.Web.Controllers.Manifest;
+
+public class ManifestController : ApiController
+{
+    private readonly ApiManifestProvider _manifestProvider;
+
+    public ManifestController(ApiManifestProvider manifestProvider)
+    {
+        _manifestProvider = manifestProvider;
+    }
+
+    [AllowAnonymous]
+    [HttpGet("about.json")]
+    [ProducesResponseType<ApiManifest>(StatusCodes.Status200OK)]
+    public IActionResult GetAboutJson()
+    {
+        return Ok(_manifestProvider.GetManifest());
+    }
+}
