35729ff03b2daa858a052d6466e12146da21f1a2
sdragos1 <dragos.suceveanu@epitech.eu>
2025-01-19 22:28:55 +0100

    feat(web): add documentation links on home page

--- web/src/app/features/home/home.page.ts
+++ web/src/app/features/home/home.page.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { IconBoxComponent } from '@components/icon-box/icon-box.component';
 import {
-  AutomationTemplateCardComponent
-} from '@features/home/automation-template-card/automation-template-card.component';
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Signal,
+} from '@angular/core';
+import { IconBoxComponent } from '@components/icon-box/icon-box.component';
+import { AutomationTemplateCardComponent } from '@features/home/automation-template-card/automation-template-card.component';
+import { AuthStore } from '@app/store';
+import { UserModel } from '@models/user.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tr-home',
@@ -10,6 +16,10 @@ import {
   standalone: true,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [IconBoxComponent, AutomationTemplateCardComponent],
+  imports: [IconBoxComponent, AutomationTemplateCardComponent, RouterLink],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  readonly #authStore = inject(AuthStore);
+
+  user: UserModel | null | undefined = this.#authStore.getUser();
+}
